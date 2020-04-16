60143647abe79e0f92e9d7622ed96c7f7ff261d2
mohamed belkahla <mohamadbr22@gmail.com>
2020-04-16 14:07:59 +0100

    ready for production

--- Chatchita/urls.py
+++ Chatchita/urls.py
@@ -11,7 +11,3 @@ urlpatterns = [
     path('comments/',include('comments.urls')),
     path('likes/',include('comments.likes_url'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
